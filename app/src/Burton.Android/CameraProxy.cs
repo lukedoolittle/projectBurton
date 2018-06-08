6c24704f460bf2d51517b332bdc9aac55da0f9f2
LDoolittle <LDoolittle@musicnotes.com>
2018-06-08 16:08:52 -0500

    added work on camera view

---
+++ app/src/Burton.Android/CameraProxy.cs
@@ -0,0 +1,31 @@
+﻿using Android.Graphics;
+
+namespace Burton.Android
+{
+    public class CameraProxy
+    {
+        private global::Android.Hardware.Camera _camera;
+
+
+        public void Start(SurfaceTexture surface)
+        {
+            _camera = global::Android.Hardware.Camera.Open();
+
+            try
+            {
+                _camera.SetPreviewTexture(surface);
+                _camera.StartPreview();
+
+            }
+            catch (Java.IO.IOException ex)
+            {
+            }
+        }
+
+        public void Shutdown()
+        {
+            _camera?.StopPreview();
+            _camera?.Release();
+        }
+    }
+}
--- app/src/Burton.Android/MainActivity.cs
+++ app/src/Burton.Android/MainActivity.cs
@@ -1,18 +1,47 @@
-﻿using Android.App;
+﻿using System.Threading.Tasks;
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.Graphics;
 using Android.Widget;
 using Android.OS;
+using Android.Util;
+using Android.Views;
+using Java.IO;
 
 namespace Burton.Android
 {
-    [Activity(Label = "Burton.Android", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "Burton.Android", Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true)]
+    public class MainActivity : CameraActivity
     {
+        private TextureView _textureView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            _textureView = new TextureView(this)
+            {
+                SurfaceTextureListener = this
+            };
+
+            SetContentView(_textureView);
+        }
+
+        public override void OnSurfaceTextureAvailable(
+            SurfaceTexture surface, 
+            int width, 
+            int height)
+        {
+            base.OnSurfaceTextureAvailable(
+                surface, 
+                width, 
+                height);
+
+            _textureView.LayoutParameters =
+                new FrameLayout.LayoutParams(width, height);
+
+            RequestCamera();
         }
     }
 }
